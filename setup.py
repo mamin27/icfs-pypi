f57c575695bd627e25e8b5329f64f319d1352c21
Marian Minar <mminar7@gmail.com>
2023-03-04 02:24:41 +0100

    move library in directory

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ classifiers = [
 
 setup(
     name = 'icfs',
-    version = '0.0.1',
+    version = '0.0.2',
     author = 'Marian Minar',
     author_email = 'mminar7@gmail.com',
     platform = 'armv7l',
-    description = 'Library create and manage file system at I2C EEPROM IC's.',
+    description = 'Library create and manage file system at I2C EEPROM ICs.',
     long_description = open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
     url = 'https://github.com/mamin27/icfs',
     license='GPLv3',
@@ -22,6 +22,11 @@ setup(
     keywords=['raspberry','I2C','filesystem','ecomet'],
     packages=find_packages(),
     install_requires=[
-    'Adafruit_PureIO>1.1.8',
+    'Adafruit_PureIO >= 1.1.8',
+    'RPI.GPIO >= 0.7.1',
+    'smbus2 >= 0.4.2',
+    'ecomet-i2c-sensors >= 0.1.6',
+    'pyyaml >= 6.0',
+    'crc >= 4.1.0',
     ]
 )
